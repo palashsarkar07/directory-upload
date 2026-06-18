117a89b19fd6d8791f244a0c3dd889104e1ea08f
Palash Sarkar <palash.sarkar@droplet.ink>
2026-06-18 18:36:38 +0530

    Added basic frontend

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/config.js', (_, res) => {
+  res.type('application/javascript');
+  res.send(`
+window.APP_CONFIG = {
+  API_BASE_URL: "${process.env.API_BASE_URL}"
+};
+`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Test client running at http://localhost:${PORT}`);
+});
